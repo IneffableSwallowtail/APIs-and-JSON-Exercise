4ab3e12ebcd885c5d92ce38fc5c596ce8bdf8e81
A.J. Miller <ajmiller1206@gmail.com>
2022-10-13 10:34:18 -0400

    first commit

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest/";
+            var swansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            for (int i = 0; i < 5; i++)
+            {
+                var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+                var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+                Console.WriteLine($"Kanye: \"{kanyeQuote}\"");
+
+                var swansonResponse = client.GetStringAsync(swansonURL).Result;
+                var swansonQuote = JArray.Parse(swansonResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+                //Wanted to use .Remove('"'), but I got an error message about startIndex not being longer than the string
+                Console.WriteLine($"Swanson: {swansonQuote}");
+            }
         }
     }
 }
